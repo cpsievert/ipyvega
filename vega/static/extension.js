0d097f460a82e4c7ab8fbb2c08679bf4f58ec262
Carson <cpsievert1@gmail.com>
2021-12-02 10:46:55 -0600

    Address #376 by not exporting/requiring under nbextension prefix and lay some ground-work for npm publishing

---
+++ vega/static/extension.js
@@ -0,0 +1,2 @@
+define((()=>{return e={474:e=>{window.require&&window.require.config({map:{"*":{"jupyter-vega":"nbextensions/jupyter-vega/index"}}}),e.exports={load_ipython_extension:function(){}}}},r={},function n(o){var t=r[o];if(void 0!==t)return t.exports;var i=r[o]={exports:{}};return e[o](i,i.exports,n),i.exports}(474);var e,r}));
+//# sourceMappingURL=extension.js.map
